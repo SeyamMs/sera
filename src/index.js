3c878825c88ca6029db2d004754de989dd0e6499
Mohammed Seyam <seyam.ms@gmail.com>
2024-08-11 04:03:01 +0300

    edits and modifications

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 import Alpine from "alpinejs"
+import intersect from "@alpinejs/intersect"
 import collapse from "@alpinejs/collapse"
 import Toolkit from "@alpine-collective/toolkit"
 
 window.Alpine = Alpine
 
+Alpine.plugin(intersect)
 Alpine.plugin(collapse)
 Alpine.plugin(Toolkit)
 Alpine.start()
+
+import EmblaCarousel from "embla-carousel"
+import AutoScroll from "embla-carousel-auto-scroll"
+import { addPrevNextBtnsClickHandlers } from "./EmblaCarouselArrowButtons"
+import { addPlayBtnListeners, addNavBtnListeners } from "./EmblaCarouselAutoScroll"
+import "./css/embla.css"
+
+const OPTIONS = { loop: true, dragFree: true, direction: "rtl" }
+
+const emblaNode = document.querySelector(".embla")
+const viewportNode = emblaNode.querySelector(".embla__viewport")
+const prevBtn = emblaNode.querySelector(".embla__button--prev")
+const nextBtn = emblaNode.querySelector(".embla__button--next")
+// const playBtn = document.querySelector(".embla__play")
+
+const emblaApi = EmblaCarousel(viewportNode, OPTIONS, [AutoScroll({ playOnInit: true, stopOnInteraction: false })])
+
+const removePrevNextBtnsClickHandlers = addPrevNextBtnsClickHandlers(emblaApi, prevBtn, nextBtn)
+const removePlayBtnListeners = addPlayBtnListeners(emblaApi)
+const removeNavBtnListeners = addNavBtnListeners(emblaApi, prevBtn, nextBtn)
+
+emblaApi.on("destroy", removePrevNextBtnsClickHandlers).on("destroy", removePlayBtnListeners).on("destroy", removeNavBtnListeners)
