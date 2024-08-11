cf1503a88b5d65f543294b4aafa57ed678ca099e
Mohammed Seyam <seyam.ms@gmail.com>
2024-08-11 21:29:47 +0300

    add countdown section

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import intersect from "@alpinejs/intersect"
 import collapse from "@alpinejs/collapse"
 import Toolkit from "@alpine-collective/toolkit"
 
+import countdown from "./countdown.js"
+
+Alpine.data("countdown", countdown)
+
 window.Alpine = Alpine
 
 Alpine.plugin(intersect)
